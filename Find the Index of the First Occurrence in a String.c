05139ebccae09739791d1e443d9eee483f4b4f43
Shashank Suman <sumanshashank616@gmail.com>
2024-06-16 17:44:29 +0530

    Update Find the Index of the First Occurrence in a String.c

--- Find the Index of the First Occurrence in a String.c
+++ Find the Index of the First Occurrence in a String.c
@@ -1,7 +1,4 @@
 //Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
-
- 
-
 int strStr(char* haystack, char* needle) {
     // Get the lengths of both haystack and needle
     int haystack_len = strlen(haystack);
